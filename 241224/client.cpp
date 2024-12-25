ba3c32f31191d67306a8c74c035dcce39fd93012
kjh <jihyeon1910@gmail.com>
2024-12-25 10:40:16 +0900

    24.12.25

--- 241224/client.cpp
+++ 241224/client.cpp
@@ -1,27 +1,9 @@
-#include <iostream>
-#include <cstring>
-#include <unistd.h>
-#include <arpa/inet.h>
-#include <sys/socket.h>
-using namespace std;
-
-void error_handling(char *message);
-
-struct test{
-    int a;
-    string b;
-    char msg[100] = "";
-    test(int a, string b) : a(a), b(b){
-        strcat(msg, to_string(a).c_str());
-        strcat(msg, ",");
-        strcat(msg, b.c_str());
-    }
-};
+#include "testHeader.h"
 
 int main(int argc, char *argv[]) {
     int sock;
     sockaddr_in serv_addr;
-    test testmsg = {1, "hello"};
+    char reqNum[BUF_SIZE] = "";
 
     int str_len = 0;
     int idx = 0;
@@ -45,16 +27,28 @@ int main(int argc, char *argv[]) {
         error_handling("connect() error");
     }
 
-    //클라이언트 통신
-    write(sock, &testmsg.msg, sizeof(testmsg.msg));
+    fputs("Input message(Q to quit) : ", stdout);
+    fgets(reqNum, BUF_SIZE, stdin);
+    if (!strcmp(reqNum, "q\n") || !strcmp(reqNum, "Q\n")) {
+        return 0;
+    }
+
+    write(sock, &reqNum, sizeof(int));
     cout << "메세지 보냄!!" << endl;
 
+    char msg[1024] = "";
+    read(sock, &msg[idx++], BUF_SIZE);
+    cout << "read_len : " << read_len << endl;
+
+    printf("Message from server : %s\n", msg);
+    cout << "메세지 받음!!" << endl;
+
+    test t;
+    t.deserializeData(msg);
+
+    shutdown(sock, SHUT_RD);
     close(sock);
+    cout << "클라이언트 끝!" << endl;
 
-}
-
-void error_handling(char *message) {
-    fputs(message, stderr);
-    fputc('\n', stderr);
-    exit(1);
+    return 0;
 }
--- 241224/server.cpp
+++ 241224/server.cpp
@@ -1,32 +1,29 @@
-#include <iostream>
-#include <cstring>
-#include <unistd.h>
-#include <arpa/inet.h>
-#include <sys/socket.h>
-using namespace std;
-
-// 에러 발생 시 내용 출력 후 프로그램 종료를 위한 함수
-void error_handling(char *message);
-
-struct test{
-    int a;
-    char b[10];
-};
+#include "testHeader.h"
+void read_childproc(int sig);
 
 int main(int argc, char *argv[]){
     int serv_sock; // 서버소켓의 파일디스크립터 저장 공간
     int clnt_sock; // 클라이언트소켓의 파일디스크립터 저장 공간
+    int str_len, state;
 
     sockaddr_in serv_addr; // 서버의 소켓 주소 정보 - IPv4형식
     sockaddr_in clnt_addr; // 클라이언트의 소켓 주소 정보
     socklen_t clnt_addr_size; // 클라이언트 소켓의 길이 정보
 
-    char msg[100] = "";
+    pid_t pid;
+    struct sigaction act;
+    char reqNum[BUF_SIZE] = "";
 
     if (argc != 2) {
         printf("Usage : %s <port>\n", argv[0]);
         exit(1);
     }
+
+    act.sa_handler = read_childproc;
+    sigemptyset(&act.sa_mask);
+    act.sa_flags = 0;
+    sigaction(SIGCHLD, &act, NULL);
+
     serv_sock = socket(PF_INET, SOCK_STREAM, 0);
     if (serv_sock == -1) {
         error_handling("socket() error");
@@ -44,25 +41,51 @@ int main(int argc, char *argv[]){
         error_handling("listen() error");
     }
 
-    clnt_addr_size = sizeof(clnt_addr);
-    clnt_sock = accept(serv_sock, (sockaddr*)&clnt_addr, &clnt_addr_size);
-    if (clnt_sock == -1) {
-        error_handling("accept() error");
+    while (1) {
+        clnt_addr_size = sizeof(clnt_addr);
+        clnt_sock = accept(serv_sock, (sockaddr*)&clnt_addr, &clnt_addr_size);
+        if (clnt_sock == -1) {
+            continue;
+        }else {
+            cout << "Client connected! > conn IP : " << inet_ntoa(clnt_addr.sin_addr) << endl;
+        }
+        pid = fork();
+        if (pid == -1) {
+            close(clnt_sock);
+            continue;
+        }
+        if (pid == 0) {
+            close(serv_sock);
+            cout << "클라이언트의 데이터 기다리는중..." << endl;
+            //서버통신
+            read(clnt_sock, &reqNum, sizeof(int));
+            cout << "클라이언트에게 데이터 받음 : " << reqNum << endl;
+
+            if (!strcmp(reqNum, "q\n") || !strcmp(reqNum, "Q\n")) {
+                break;
+            }
+
+            test *t = new test(1,"HImynameisjihyeon", "jihyeon222221231231");
+            string buf = t->serializeData();
+            buf += "^%^" + string(reqNum);
+
+            write(clnt_sock, buf.c_str(), buf.size());
+            shutdown(clnt_sock, SHUT_WR);
+            cout << "클라이언트에게 데이터 보냄 : " << buf << endl;
+            delete t;
+        }else {
+            close(clnt_sock);
+        }
     }
-
-    //서버통신
-    read(clnt_sock, &msg, sizeof(msg));
-
-    cout << "메세지 받음!!" << endl;
-    cout << "msg : " << msg << endl;
-
-    close(clnt_sock);
     close(serv_sock);
+    cout << "서버 끝!" << endl;
+
     return 0;
 }
 
-void error_handling(char *message) {
-    fputs(message, stderr);
-    fputc('\n', stderr);
-    exit(1);
+void read_childproc(int sig) {
+    pid_t pid;
+    int status;
+    pid = waitpid(-1, &status, WNOHANG);
+    cout << "remove proc id : " << pid << endl;
 }
